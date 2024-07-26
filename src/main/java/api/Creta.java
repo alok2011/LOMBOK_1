05ee2a96b3c94fb86107f9c921349bbb8af3f649
alok2011 <alokranjantiwaricse@gmail.com>
2024-07-26 14:21:38 +0530

    update

--- src/main/java/api/Creta.java
+++ src/main/java/api/Creta.java
@@ -1,11 +1,6 @@
 package api;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
 //@Getter
 //@Setter
--- src/main/java/api/EmployeeValue.java
+++ src/main/java/api/EmployeeValue.java
@@ -1,8 +1,10 @@
 package api;
 
+import lombok.AllArgsConstructor;
 import lombok.Value;
 
 @Value//(Equivalent to @Getter @FieldDefaults(makeFinal=true, level=AccessLevel.PRIVATE) @AllArgsConstructor @ToString @EqualsAndHashCode. )
+@AllArgsConstructor
 public class EmployeeValue 
 {
 	String name;
